a671102d30758c797e378d4f7ccb47b2e3a16074
Stef Kluskens <kluskens_stef@hotmail.com>
2024-11-08 12:03:47 +0100

    Created project + Created project browser UI
    Created the project. Engine is a C++ static library. Editor is a C# WPF project.
    Setup so that when the projects are build, the files are in the same directory, to make for easier linking.
    
    Project browser UI made using WPF. Separate user controls for the open/create project panels

---
+++ GingerEngine/GingerEditor/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using GingerEditor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GingerEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+
+            OpenProjectBrowser();
+        }
+
+        private void OpenProjectBrowser()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
